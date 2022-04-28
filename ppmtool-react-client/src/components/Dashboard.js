8171f59685bbc23dad7a4d18d62883fc6c5e0f46
Saloni Soundatte <saloni_soundatte@persistent.com>
2022-04-28 14:35:49 +0530

    React Started

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
+  }
+}
+export default Dashboard;
