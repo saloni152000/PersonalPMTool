e797b1b0bad4dcfd1e670e6b2ab887981b79935e
Saloni Soundatte <saloni_soundatte@persistent.com>
2022-04-28 17:49:16 +0530

    React Router

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,26 @@
+import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
