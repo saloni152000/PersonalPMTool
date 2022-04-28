dbf09779a8b4df0ee805e4b6ae08689facbc1e53
Saloni Soundatte <saloni_soundatte@persistent.com>
2022-04-28 12:55:28 +0530

    Find all and delete Projects

--- ppmtool/src/main/java/com/saloni/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/saloni/ppmtool/services/ProjectService.java
@@ -22,4 +22,27 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
 		}
 	}
+	
+	public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+        return project;
+	}
+	public Iterable<Project> findAllProject(){
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjecByIdentifier(String projectid) {
+		Project project=projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+		
+		if(project==null) {
+            throw  new  ProjectIdException("Cannot Project with ID '"+projectid+"'. This project does not exist");
+		}
+		projectRepository.delete(project);
+	}
 }
--- ppmtool/src/main/java/com/saloni/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/saloni/ppmtool/web/ProjectController.java
@@ -11,6 +11,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,4 +43,25 @@ public class ProjectController {
 		Project project1=projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 	}
+	
+	 @GetMapping("/{projectId}")
+	    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+	        Project project = projectService.findProjectByIdentifier(projectId);
+
+	        return new ResponseEntity<Project>(project, HttpStatus.OK);
+	    }
+	 
+	 @GetMapping("/all")
+	 public Iterable<Project>getAllProjects(){
+		 return projectService.findAllProject();
+	 }
+	 
+	 @DeleteMapping("/{projectId}")
+	 public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+		 projectService.deleteProjecByIdentifier(projectId);
+		 
+	    return new ResponseEntity<String>("Project with ID: '"+projectId+"' was deleted", HttpStatus.OK);
+
+	 }
 }
