4ae7148c5d0297d15b8b756c7ba480f587b85242
Saloni Soundatte <saloni_soundatte@persistent.com>
2022-04-29 12:17:45 +0530

    Redux Store

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,20 +5,24 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
 
-          <Routes>
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route exact path="/addProject" element={<AddProject />} />
-          </Routes>
-        </div>
-      </Router>
+            <Routes>
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/addProject" element={<AddProject />} />
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
